25514592ef2347bc082c759191f6de4c7d53b478
fabiocsantos <68649715+fabiocsantos@users.noreply.github.com>
2022-03-21 01:13:25 +0000

    Update settings.py

--- rointesdk/settings.py
+++ rointesdk/settings.py
@@ -7,8 +7,8 @@ AUTH_TIMEOUT_SECONDS = 15
 
 AUTH_REFRESH_ENDPOINT = "https://securetoken.googleapis.com/v1/token"
 
-FIREBASE_APP_KEY = "AIzaSyBi1DFJlBr9Cezf2BwfaT-PRPYmi3X3pdA"
-FIREBASE_DEFAULT_URL = "https://elife-prod.firebaseio.com"
+FIREBASE_APP_KEY = "AIzaSyDfqBq3AfIg1wPjuHse3eiXqeDIxnhvp6U"
+FIREBASE_DEFAULT_URL = "https://oem2-elife-cloud-prod-default-rtdb.firebaseio.com"
 FIREBASE_INSTALLATIONS_PATH = "/installations2.json"
 FIREBASE_DEVICES_PATH_BY_ID = "/devices/{}.json"
 FIREBASE_DEVICE_DATA_PATH_BY_ID = "/devices/{}/data.json"
